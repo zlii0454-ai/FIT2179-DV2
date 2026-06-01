d3830937f63658283cc2fc08d8be86eb1b16a6eb
zlii0454-ai <zlii0454@student.monash.edu>
2026-06-01 10:38:38 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 vegaEmbed(
   "#line_chart",
-  "specs/student_growth.json"
+  "specs/student_growth.json",
+  { actions: false }
 ).catch(console.error);
 
 vegaEmbed(
   "#country_lollipop",
-  "specs/country_lollipop.json"
+  "specs/country_lollipop.json",
+  { actions: false }
 ).catch(console.error);
